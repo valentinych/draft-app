5d8a3d556ad3e0f388cb2004695d4ca86f2ffcfe
valetinych <aharodnik@gmail.com>
2025-08-20 11:56:03 +0200

    Remove placeholder Top-4 draft route

--- draft_app/home.py
+++ draft_app/home.py
@@ -1,15 +1,8 @@
 from flask import Blueprint, render_template
-from .auth import load_auth_users
-from .state import init_ucl, init_epl
-from .config import BASE_DIR
 
 bp = Blueprint("home", __name__)
+
 
 @bp.route("/")
 def index():
     return render_template("home.html")
-
-# Плейсхолдер для Top-4
-@bp.route("/top4")
-def top4():
-    return render_template("empty_draft.html", draft_name="Топ-4 Драфт")
